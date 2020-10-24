d19981e6c4bbe17714dad099f47baa8c41b3aed5
RYSH-XD <57472967+RYSH-XD@users.noreply.github.com>
2020-10-24 17:30:19 +0800

    Add files via upload

---
+++ plot_bbox.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import os
+
+image_root_dir='path to image'
+label_root_dir='path to label'
+labeled_image_root_dir='path to result'
+
+if not os.path.exists(labeled_image_root_dir):
+    os.makedirs(labeled_image_root_dir)
+
+#image_root_list=os.listdir(image_root_dir)
+label_root_list=os.listdir(label_root_dir)
+
+for i,name in enumerate(label_root_list):
+    print ('plot '+name)
+    
+    img=cv.imread(image_root_dir+'/'+name[:-4]+'.tif')
+    f = open(label_root_dir+'/'+name,'r')
+    for line in open(label_root_dir+'/'+name):
+            
+        class_name=line.split(' ')[0]
+        coordinate=[]
+        coordinate.append(int(line.split(' ')[1]))
+        coordinate.append(int(line.split(' ')[2]))
+        coordinate.append(int(line.split(' ')[3]))
+        coordinate.append(int(line.split(' ')[4]))
+        coordinate.append(int(line.split(' ')[5]))
+        coordinate.append(int(line.split(' ')[6]))
+        coordinate.append(int(line.split(' ')[7]))
+        coordinate.append(int(line.split(' ')[8]))
+        for counter in range(3):
+            counter=counter*2
+            ptStart = (coordinate[counter], coordinate[counter+1])
+            ptEnd = (coordinate[counter+2], coordinate[counter+3])
+            point_color = (0, 255, 0) # BGR
+            thickness = 1 
+            lineType = 4
+            cv.line(img, ptStart, ptEnd, point_color, thickness, lineType)
+        #counter=3:
+        ptStart = (coordinate[6], coordinate[7])
+        ptEnd = (coordinate[0], coordinate[1])
+        point_color = (0, 255, 0) # BGR
+        thickness = 1 
+        lineType = 4
+        cv.line(img, ptStart, ptEnd, point_color, thickness, lineType)
+        #lable name
+        font = cv.FONT_HERSHEY_COMPLEX
+        cv.putText(img, class_name_config, (coordinate[0], coordinate[1]), font, 0.5, (0, 0, 255), 1)
+
+    f.close()
+    cv.imwrite(labeled_image_root_dir+'/'+name[:-4]+'.tif', img)
+    
